b4142da032e78d1f53b6b1902d6580f8b17197b0
Jukka Taimisto <jtaimisto@gmail.com>
2018-10-17 19:51:25 +0300

    Add receiving of JSON encoded found devices data to listener.

--- listener/main.go
+++ listener/main.go
@@ -7,16 +7,18 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"gitlab.com/jtaimisto/bluewalker/hci"
 	"gitlab.com/jtaimisto/bluewalker/ruuvi"
 )
 
 var path string
+var ruuviMode bool
 
 func init() {
 	flag.StringVar(&path, "unix", "", "Unix socket path to listen for connections")
-
+	flag.BoolVar(&ruuviMode, "ruuvi", false, "Expect to receive ruuvi data")
 }
 
 func main() {
@@ -45,19 +47,34 @@ func main() {
 
 	for line, err := rd.ReadString('\n'); err == nil; line, err = rd.ReadString('\n') {
 
-		fmt.Printf("Received \"%s\"", line)
-		ruuvi := struct {
-			Device hci.BtAddress `json:"device"`
-			Rssi   int8          `json:"rssi"`
-			Values *ruuvi.Data   `json:"sensors"`
-		}{Device: hci.BtAddress{}, Values: &ruuvi.Data{}}
-		jerr := json.Unmarshal([]byte(line), &ruuvi)
-		if jerr != nil {
-			fmt.Fprintf(os.Stderr, "Error while reading ruuvi data: %s\n", jerr.Error())
-			continue
+		//fmt.Printf("Received \"%s\"", line)
+		if ruuviMode {
+			ruuvi := struct {
+				Device hci.BtAddress `json:"device"`
+				Rssi   int8          `json:"rssi"`
+				Values *ruuvi.Data   `json:"sensors"`
+			}{Device: hci.BtAddress{}, Values: &ruuvi.Data{}}
+			jerr := json.Unmarshal([]byte(line), &ruuvi)
+			if jerr != nil {
+				fmt.Fprintf(os.Stderr, "Error while reading ruuvi data: %s\n", jerr.Error())
+				continue
+			}
+			fmt.Printf("Got: %+v\n", ruuvi)
+			fmt.Printf("Values is: %+v\n", ruuvi.Values)
+		} else {
+			devices := []struct {
+				Structures []*hci.AdStructure `json:"data"`
+				LastSeen   time.Time          `json:"last"`
+				Rssi       int8               `json:"rssi"`
+				Types      []hci.AdvType      `json:"types"`
+				Device     hci.BtAddress      `json:"device"`
+			}{}
+			if jerr := json.Unmarshal([]byte(line), &devices); err != nil {
+				fmt.Fprintf(os.Stderr, "Error while reading found devices data: %s\n", jerr.Error())
+				continue
+			}
+			fmt.Printf("Got: %+v\n", devices)
 		}
-		fmt.Printf("Got: %+v\n", ruuvi)
-		fmt.Printf("Values is: %+v\n", ruuvi.Values)
 	}
 	fmt.Printf("Shutting down")
 }
