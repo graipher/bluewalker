798df930249e587ccf406f9ed62d73c879e42417
Jukka Taimisto <jtaimisto@gmail.com>
2018-11-04 22:10:43 +0200

    Add observer mode to listener
    Print out information about found devices in both, observer and
    collector, modes.

--- listener/main.go
+++ listener/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"gitlab.com/jtaimisto/bluewalker/hci"
@@ -15,10 +16,37 @@ import (
 
 var path string
 var ruuviMode bool
+var observer bool
 
 func init() {
 	flag.StringVar(&path, "unix", "", "Unix socket path to listen for connections")
 	flag.BoolVar(&ruuviMode, "ruuvi", false, "Expect to receive ruuvi data")
+	flag.BoolVar(&observer, "observer", false, "Start listener in observer mode")
+}
+
+type device struct {
+	Structures []*hci.AdStructure `json:"data"`
+	LastSeen   time.Time          `json:"last"`
+	Rssi       int8               `json:"rssi"`
+	Types      []hci.AdvType      `json:"types"`
+	Device     hci.BtAddress      `json:"device"`
+}
+
+func (dev *device) String() string {
+
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("Device: %s RSSI: %d \n", dev.Device.String(), dev.Rssi))
+	sb.WriteString("Adv types: ")
+	for _, t := range dev.Types {
+		sb.WriteString(fmt.Sprintf("%s ", t.String()))
+	}
+	sb.WriteString("\n")
+	sb.WriteString("Adv Data:\n")
+	for _, s := range dev.Structures {
+		sb.WriteString(fmt.Sprintf("\t%s\n", s.String()))
+	}
+	sb.WriteString("\n")
+	return sb.String()
 }
 
 func main() {
@@ -62,18 +90,29 @@ func main() {
 			fmt.Printf("Got: %+v\n", ruuvi)
 			fmt.Printf("Values is: %+v\n", ruuvi.Values)
 		} else {
-			devices := []struct {
-				Structures []*hci.AdStructure `json:"data"`
-				LastSeen   time.Time          `json:"last"`
-				Rssi       int8               `json:"rssi"`
-				Types      []hci.AdvType      `json:"types"`
-				Device     hci.BtAddress      `json:"device"`
-			}{}
-			if jerr := json.Unmarshal([]byte(line), &devices); err != nil {
+
+			var data interface{}
+			var printer func(interface{})
+			if !observer {
+				data = &[]*device{}
+				printer = func(in interface{}) {
+					list := in.(*[]*device)
+					for _, l := range *list {
+						fmt.Printf("%s", l.String())
+					}
+				}
+			} else {
+				data = &device{}
+				printer = func(in interface{}) {
+					d := in.(*device)
+					fmt.Printf("%s", d.String())
+				}
+			}
+			if jerr := json.Unmarshal([]byte(line), data); err != nil {
 				fmt.Fprintf(os.Stderr, "Error while reading found devices data: %s\n", jerr.Error())
 				continue
 			}
-			fmt.Printf("Got: %+v\n", devices)
+			printer(data)
 		}
 	}
 	fmt.Printf("Shutting down")
