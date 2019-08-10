29f69effbca6bbbb126101144d4bc11ed479cbde
Jukka Taimisto <jtaimisto@gmail.com>
2019-08-10 20:18:00 +0300

    Version 0.2.1

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	//BluewalkerVersion contains the current version string
-	BluewalkerVersion string = "0.2.1-dev"
+	BluewalkerVersion string = "0.2.1"
 )
 
 // Command line settings
