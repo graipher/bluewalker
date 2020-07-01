23e80c81a883aff651aec343d4df8e036c5b983a
Jukka Taimisto <jtaimisto@gmail.com>
2020-07-01 18:35:10 +0300

    Version number bump

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 
 const (
 	//BluewalkerVersion contains the current version string
-	BluewalkerVersion string = "0.2.2"
+	BluewalkerVersion string = "0.2.3-dev"
 )
 
 // Command line settings
