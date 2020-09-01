f0946ad5ca470de2efe30d5770a3bcbd1f282dc7
Jukka Taimisto <jtaimisto@gmail.com>
2020-09-01 20:14:16 +0300

    Use AdDataFilter for vendor specific filter
    No need to have specific filter for vendor specific data as it is only a special
    case of more generic AdData filter.

--- filter/filter.go
+++ filter/filter.go
@@ -41,29 +41,12 @@ func ByPartialAddress(buf []byte) AdFilter {
 	return &partAddrFilter{part: buf}
 }
 
-type vendorFilter struct {
-	preamble []byte
-}
-
-func (v *vendorFilter) Filter(report *hci.AdvertisingReport) bool {
-	ret := false
-	for _, data := range report.Data {
-		if data.Typ == hci.AdManufacturerSpecific {
-			if len(data.Data) < len(v.preamble) {
-				continue
-			}
-			return bytes.Equal(data.Data[:len(v.preamble)], v.preamble)
-		}
-	}
-	return ret
-}
-
 //ByVendor returns AdFilter which can be used to filter Advertising Reports
 //based on the start of its vendor specific Advertising Data. Filter
 //passes Advertising Reports which have vendor -specific advertising data and
 //the first bytes of the vendor specific data match the given preamble.
 func ByVendor(preamble []byte) AdFilter {
-	return &vendorFilter{preamble: preamble}
+	return &adDataFilter{typ: hci.AdManufacturerSpecific, preamble: preamble}
 }
 
 type adDataFilter struct {
